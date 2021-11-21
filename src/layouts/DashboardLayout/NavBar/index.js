28f40125e19537857023112a650424a0a2a63ea2
louiscklaw <louiscklaw@gmail.com>
2021-11-21 17:02:44 +0800

    update fix nav bar,

--- src/layouts/DashboardLayout/NavBar/index.js
+++ src/layouts/DashboardLayout/NavBar/index.js
@@ -1,17 +1,7 @@
-import React, { useEffect } from "react";
-import { Link as RouterLink, useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
-import {
-  Avatar,
-  Box,
-  Button,
-  Divider,
-  Drawer,
-  Hidden,
-  List,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
+import React, { useEffect } from 'react';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Avatar, Box, Button, Divider, Drawer, Hidden, List, Typography, makeStyles } from '@material-ui/core';
 import {
   AlertCircle as AlertCircleIcon,
   BarChart as BarChartIcon,
@@ -21,13 +11,13 @@ import {
   User as UserIcon,
   UserPlus as UserPlusIcon,
   Users as UsersIcon,
-} from "react-feather";
-import NavItem from "./NavItem";
+} from 'react-feather';
+import NavItem from './NavItem';
 
 const user = {
-  avatar: "/static/images/avatars/avatar_6.png",
-  jobTitle: "Senior Developer",
-  name: "Katarina Smith",
+  avatar: '/static/images/avatars/avatar_6.png',
+  jobTitle: 'Senior Developer',
+  name: 'Katarina Smith',
 };
 
 const ENV_PUBLIC_URL = process.env.PUBLIC_URL;
@@ -36,12 +26,12 @@ const items = [
   {
     href: `${ENV_PUBLIC_URL}/app/datatyles`,
     icon: BarChartIcon,
-    title: "datatyles",
+    title: 'datatyles',
   },
   {
     href: `${ENV_PUBLIC_URL}/app/dashboard`,
     icon: BarChartIcon,
-    title: "Dashboard",
+    title: 'Dashboard',
   },
   // {
   //   href: `${ENV_PUBLIC_URL}/app/customers`,
@@ -66,7 +56,7 @@ const items = [
   {
     href: `${ENV_PUBLIC_URL}/login`,
     icon: LockIcon,
-    title: "Login",
+    title: 'Login',
   },
   // {
   //   href: `${ENV_PUBLIC_URL}/register`,
@@ -87,10 +77,10 @@ const useStyles = makeStyles(() => ({
   desktopDrawer: {
     width: 256,
     top: 64,
-    height: "calc(100% - 64px)",
+    height: 'calc(100% - 64px)',
   },
   avatar: {
-    cursor: "pointer",
+    cursor: 'pointer',
     width: 64,
     height: 64,
   },
@@ -110,12 +100,7 @@ const NavBar = ({ onMobileClose, openMobile }) => {
   const content = (
     <Box height="100%" display="flex" flexDirection="column">
       <Box alignItems="center" display="flex" flexDirection="column" p={2}>
-        <Avatar
-          className={classes.avatar}
-          component={RouterLink}
-          src={user.avatar}
-          to="/app/account"
-        />
+        <Avatar className={classes.avatar} component={RouterLink} src={user.avatar} to="/app/account" />
         <Typography className={classes.name} color="textPrimary" variant="h5">
           {user.name}
         </Typography>
@@ -127,12 +112,7 @@ const NavBar = ({ onMobileClose, openMobile }) => {
       <Box p={2}>
         <List>
           {items.map((item) => (
-            <NavItem
-              href={item.href}
-              key={item.title}
-              title={item.title}
-              icon={item.icon}
-            />
+            <NavItem href={item.href} key={item.title} title={item.title} icon={item.icon} />
           ))}
         </List>
       </Box>
@@ -145,12 +125,7 @@ const NavBar = ({ onMobileClose, openMobile }) => {
           Upgrade to PRO version and access 20 more screens
         </Typography>
         <Box display="flex" justifyContent="center" mt={2}>
-          <Button
-            color="primary"
-            component="a"
-            href="https://react-material-kit.devias.io"
-            variant="contained"
-          >
+          <Button color="primary" component="a" href="https://react-material-kit.devias.io" variant="contained">
             See PRO version
           </Button>
         </Box>
@@ -166,18 +141,12 @@ const NavBar = ({ onMobileClose, openMobile }) => {
           classes={{ paper: classes.mobileDrawer }}
           onClose={onMobileClose}
           open={openMobile}
-          variant="temporary"
-        >
+          variant="temporary">
           {content}
         </Drawer>
       </Hidden>
-      <Hidden mdDown>
-        <Drawer
-          anchor="left"
-          classes={{ paper: classes.desktopDrawer }}
-          open
-          variant="persistent"
-        >
+      <Hidden lgDown>
+        <Drawer anchor="left" classes={{ paper: classes.desktopDrawer }} open variant="persistent">
           {content}
         </Drawer>
       </Hidden>
