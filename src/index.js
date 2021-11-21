5f6351319a9bd2b87c693881cba60f55878dd502
louiscklaw <louiscklaw@gmail.com>
2021-11-21 23:20:07 +0800

    update baseurl,

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppContext>
         <App />
       </AppContext>
