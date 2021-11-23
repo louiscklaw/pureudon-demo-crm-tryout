fe574b40a88b861c9cca8ff147278948ee70aa50
louiscklaw <louiscklaw@gmail.com>
2021-11-23 11:29:25 +0800

    update .htaccess,

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -13,3 +13,20 @@ ncftpput -R -v -m \
   $PUREUDON_FTP_HOST \
   $CRM_DEMO_DIR \
   $PWD/build/*
+
+ncftpput -R -v -m \
+  -u $PUREUDON_FTP_USERNAME \
+  -p $PUREUDON_FTP_PASSWORD \
+  -Y "rm -rf $CRM_DEMO_DIR" \
+  $PUREUDON_FTP_HOST \
+  $CRM_DEMO_DIR \
+  $PWD/overlay/.htaccess
+
+
+ncftpput -R -v -m \
+  -u $PUREUDON_FTP_USERNAME \
+  -p $PUREUDON_FTP_PASSWORD \
+  -Y "rm -rf $CRM_DEMO_DIR" \
+  $PUREUDON_FTP_HOST \
+  $CRM_DEMO_DIR \
+  $PWD/build/index.html
