e62a81b5307da4b5f89697db6c9a2673c4aa16ac
louiscklaw <louiscklaw@gmail.com>
2021-12-07 16:02:51 +0800

    update,

--- src/components/ReactTablePaginationHelloworld/index.js
+++ src/components/ReactTablePaginationHelloworld/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import matchSorter from 'match-sorter';
 
-import styled from 'styled-components';
-import { useTable, usePagination } from 'react-table';
+import { useTable, usePagination, useFilters, useGlobalFilter, useAsyncDebounce } from 'react-table';
 
 import {
   Box,
@@ -28,11 +28,188 @@ const useStyles = makeStyles((theme) => ({
   avatar: { marginRight: theme.spacing(2) },
 }));
 
+// Define a custom filter filter function!
+function filterGreaterThan(rows, id, filterValue) {
+  return rows.filter((row) => {
+    const rowValue = row.values[id];
+    return rowValue >= filterValue;
+  });
+}
+
+function SelectColumnFilter({ column: { filterValue, setFilter, preFilteredRows, id } }) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set();
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id]);
+    });
+    return [...options.values()];
+  }, [id, preFilteredRows]);
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}>
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function NumberRangeColumnFilter({ column: { filterValue = [], preFilteredRows, setFilter, id } }) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    preFilteredRows.forEach((row) => {
+      min = Math.min(row.values[id], min);
+      max = Math.max(row.values[id], max);
+    });
+    return [min, max];
+  }, [id, preFilteredRows]);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}>
+      <input
+        value={filterValue[0] || ''}
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]]);
+        }}
+        placeholder={`Min (${min})`}
+        style={{ width: '70px', marginRight: '0.5rem' }}
+      />
+      to
+      <input
+        value={filterValue[1] || ''}
+        type="number"
+        onChange={(e) => {
+          const val = e.target.value;
+          setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined]);
+        }}
+        placeholder={`Max (${max})`}
+        style={{ width: '70px', marginLeft: '0.5rem' }}
+      />
+    </div>
+  );
+}
+
+function SliderColumnFilter({ column: { filterValue, setFilter, preFilteredRows, id } }) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0;
+    preFilteredRows.forEach((row) => {
+      min = Math.min(row.values[id], min);
+      max = Math.max(row.values[id], max);
+    });
+    return [min, max];
+  }, [id, preFilteredRows]);
+
+  return (
+    <>
+      <input
+        type="range"
+        min={min}
+        max={max}
+        value={filterValue || min}
+        onChange={(e) => {
+          setFilter(parseInt(e.target.value, 10));
+        }}
+      />
+      <button onClick={() => setFilter(undefined)}>Off</button>
+    </>
+  );
+}
+
+function DefaultColumnFilter({ column: { filterValue, preFilteredRows, setFilter } }) {
+  const count = preFilteredRows.length;
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  );
+}
+
+function GlobalFilter({ preGlobalFilteredRows, globalFilter, setGlobalFilter }) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => setGlobalFilter(value || undefined), 200);
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  );
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [(row) => row.values[id]] });
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = (val) => !val;
+
 // Let's add a fetchData method to our Table component that will be used to fetch
 // new data when pagination state changes
 // We can also add a loading state to let our table know it's loading new data
 function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPageCount }) {
   const classes = useStyles();
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  );
+
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter((row) => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      },
+    }),
+    []
+  );
   const {
     getTableProps,
     getTableBodyProps,
@@ -47,8 +224,11 @@ function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPage
     nextPage,
     previousPage,
     setPageSize,
+    visibleColumns,
+    preGlobalFilteredRows,
     // Get the state from the instance
-    state: { pageIndex, pageSize },
+    state,
+    setGlobalFilter,
   } = useTable(
     {
       columns,
@@ -59,7 +239,11 @@ function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPage
       // This means we'll also have to provide our own
       // pageCount.
       pageCount: controlledPageCount,
+      defaultColumn,
+      filterTypes,
     },
+    useFilters,
+    useGlobalFilter,
     usePagination
   );
 
@@ -78,8 +262,8 @@ function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPage
 
   // Listen for changes in pagination and use the state to fetch our new data
   React.useEffect(() => {
-    fetchData({ pageIndex, pageSize });
-  }, [fetchData, pageIndex, pageSize]);
+    fetchData({ pageIndex: state.pageIndex, pageSize: state.pageSize });
+  }, [fetchData, state.pageIndex, state.pageSize]);
 
   // Render the UI for your table
 
@@ -94,10 +278,22 @@ function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPage
               {headerGroups.map((headerGroup) => (
                 <TableRow {...headerGroup.getHeaderGroupProps()}>
                   {headerGroup.headers.map((column) => (
-                    <TableCell {...column.getHeaderProps()}>{column.render('Header')}</TableCell>
+                    <TableCell {...column.getHeaderProps()}>
+                      {column.render('Header')}
+                      <div>{column.canFilter ? column.render('Filter') : null}</div>
+                    </TableCell>
                   ))}
                 </TableRow>
               ))}
+              {/* <TableRow>
+                <TableCell colSpan={visibleColumns.length} style={{ textAlign: 'left' }}>
+                  <GlobalFilter
+                    preGlobalFilteredRows={preGlobalFilteredRows}
+                    globalFilter={state.globalFilter}
+                    setGlobalFilter={setGlobalFilter}
+                  />
+                </TableCell>
+              </TableRow> */}
             </TableHead>
             <TableBody {...getTableBodyProps()}>
               {page.map((row, i) => {
@@ -115,18 +311,24 @@ function MuiTable({ columns, data, fetchData, loading, pageCount: controlledPage
         </PerfectScrollbar>
         <TablePagination
           component="div"
-          count={controlledPageCount * pageSize}
+          count={controlledPageCount * state.pageSize}
           onChangePage={(e) => {}}
           onPageChange={(e, page) => handlePageChange(e, page)}
           onRowsPerPageChange={handleLimitChange}
-          page={pageIndex}
-          rowsPerPage={pageSize}
+          page={state.pageIndex}
+          rowsPerPage={state.pageSize}
           rowsPerPageOptions={[10, 20]}
         />
       </Card>
 
       <pre>
-        <code>{JSON.stringify({ pageIndex, pageSize, pageCount, canNextPage, canPreviousPage }, null, 2)}</code>
+        <code>
+          {JSON.stringify(
+            { pageIndex: state.pageIndex, pageSize: state.pageSize, pageCount, canNextPage, canPreviousPage },
+            null,
+            2
+          )}
+        </code>
       </pre>
     </>
   );
@@ -141,11 +343,13 @@ function App() {
       {
         Header: 'Info',
         columns: [
-          { Header: 'varchartype', accessor: 'varchartype' },
-          { Header: 'inttype', accessor: 'inttype' },
-          { Header: 'yeartype', accessor: 'yeartype' },
-          { Header: 'datetype', accessor: 'datetype' },
-          { Header: 'datetimetype', accessor: 'datetimetype' },
+          { Header: 'varchartype', accessor: 'varchartype', filter: 'fuzzyText' },
+          { Header: 'inttype', accessor: 'inttype', Filter: NumberRangeColumnFilter, filter: 'between' },
+          { Header: 'inttype1', accessor: 'inttype1', Filter: SliderColumnFilter, filter: 'equals' },
+          { Header: 'inttype2', accessor: 'inttype2', Filter: SliderColumnFilter, filter: filterGreaterThan },
+          { Header: 'yeartype', accessor: 'yeartype', Filter: SelectColumnFilter, filter: 'includes' },
+          { Header: 'datetype', accessor: 'datetype', filter: 'fuzzyText' },
+          { Header: 'datetimetype', accessor: 'datetimetype', filter: 'fuzzyText' },
         ],
       },
     ],
@@ -167,22 +371,20 @@ function App() {
     // Set the loading state
     setLoading(true);
 
-    use_mutate_datatypes
-      .mutateAsync(({ pageSize, pageIndex }) => {})
-      .then((res) => {
-        let serverData = res.data;
-        if (fetchId === fetchIdRef.current) {
-          const startRow = pageSize * pageIndex;
-          const endRow = startRow + pageSize;
-          setData(serverData.slice(startRow, endRow));
+    use_mutate_datatypes.mutateAsync({ pageSize, pageIndex }).then((res) => {
+      let serverData = res.data;
+      if (fetchId === fetchIdRef.current) {
+        const startRow = pageSize * pageIndex;
+        const endRow = startRow + pageSize;
+        setData(serverData.slice(startRow, endRow));
 
-          // Your server could send back total page count.
-          // For now we'll just fake it, too
-          setPageCount(Math.ceil(serverData.length / pageSize));
+        // Your server could send back total page count.
+        // For now we'll just fake it, too
+        setPageCount(Math.ceil(serverData.length / pageSize));
 
-          setLoading(false);
-        }
-      });
+        setLoading(false);
+      }
+    });
   }, []);
 
   return (
